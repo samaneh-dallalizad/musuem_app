492257222bca2b58dbe27f8f0c17637652b872c0
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-04-25 13:53:27 +0200

    send new js and html files

---
+++ assets/js/comment.js
@@ -0,0 +1,43 @@
+
+ 
+document.getElementById('send_msg').addEventListener('click', function(event){
+    event.preventDefault();
+    const name=document.getElementById('name');
+    const msg=document.getElementById('message');
+    const nameValue=name.value;
+    const msgValue=msg.value;
+
+    if(formValidation(nameValue,msgValue)){
+        console.log("form_validation");
+    }else{
+        const comment = document.createElement('div');
+        const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+        
+        h3.innerHTML = `${nameValue} said:`;
+        p.innerHTML = msgValue;
+        comment.classList.add('user_comment');
+        comment.appendChild(h3);
+        comment.appendChild(p);
+        document.getElementById("comments").appendChild(comment); 
+    }
+
+       
+      
+});
+
+function formValidation(name,msg){
+    if (!name || !msg) {
+        alert('You forgot to fill in your name or message!')
+        return true;
+      }
+    
+      if(msg.length > 280) {
+        alert('Your comment is too long')
+        return true
+      }
+    
+      return false
+}
+
+
